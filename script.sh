649fba058c90f8ccd7989cedff945f6920068a24
okromrey <okromrey05@gmail.com>
2025-02-26 09:45:52 -0600

    initial commit

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --partition=week                        #Partition to submit to
+#SBATCH --time=0-00:00:30                       #Time limit for this job
+#SBATCH --nodes=1                               #Nodes to be used for this job during runtime. Use MPI jobs with multiple nodes.
+#SBATCH --ntasks-per-node=1                     #Number of CPUs. Cannot be greater than number of CPUs on the node.
+#SBATCH --mem=512                               #Total memory for this job
+#SBATCH --job-name="LLM Finetuning"             #Name of this job in work queue
+#SBATCH --output=llm_finetuning.out             #Output file name
+#SBATCH --error=llm_finetuning.err              #Error file name
+#SBATCH --mail-type=END                         #Email notification type (BEGIN, END, FAIL, ALL). To have multiple use a comma separated list. i.e END,FAIL.
+#SBATCH partition=GPU
+#SBATCH gpus=1
+
+# Job Commands Below
+!pip install unsloth vllm
+!pip install --upgrade pillow
+echo "Hello from $(hostname)"
+python3 finetuning.py
